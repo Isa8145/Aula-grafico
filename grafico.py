627c1c6311612d67f047fa8a3b7d6148d260b7db
Isa8145 <iisabella.psm@gmail.com>
2025-05-29 11:20:58 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -4,6 +4,6 @@ import streamlit as st
 try: 
   tot_registros = df.count() ['total']
 except: 
-  df = pd.read_csv('constructor_results_2025.csv')
+  df = pd.read_csv('https://www.kaggle.com/datasets/umerhaddii/f1-japanese-grand-prix-2025#')
 
 st.bar_chart(df, x='month', y='total')
