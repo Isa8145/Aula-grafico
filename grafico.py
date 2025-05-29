b8c8e17b089186b9d72e3ba8f3738045c8c4c2b0
Isa8145 <iisabella.psm@gmail.com>
2025-05-29 16:11:20 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -6,6 +6,8 @@ df = pd.read_csv('tempo_uso_apps.csv')
 st.title("📱 Tempo médio gasto por dia em aplicativos")
 st.caption("Dados fictícios, mas provavelmente bem reais pra nossa vida 😅")
 
+st.caption("Tempo médio diário gasto em aplicativos (em horas)")
+
 st.dataframe(df)
 
 st.bar_chart(df.set_index('dia'))
